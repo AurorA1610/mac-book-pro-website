dfd82e6640c883438d97d4dfbf0a7b72ad7aa2c2
AurorA1610 <auroralroy2003@gmail.com>
2021-08-21 19:45:57 +0600

    done

--- js/app.js
+++ js/app.js
@@ -1,24 +1,38 @@
-// document.getElementById('first-memory').addEventListener('click', function(){
-//     memoryCost = document.getElementById('memory-cost');
-//     memoryCostAmount = 200;
-//     memoryCost.innerText = memoryCostAmount;
-// })
-function updatePartsCost(extraPartID, partCostID, price) {
+//  function to update cost list
+function updatePartsCost( extraPartID, partCostID, price ) {
     document.getElementById( extraPartID ).addEventListener( 'click', function() {
-        partCost = document.getElementById(partCostID);
+
+        // updating the part's cost
+        const partCost = document.getElementById( partCostID );
         partCost.innerText = price;
+
+        // calling function for updating the total cost
         updateTotalCost();
     } )
 }
+
+
+// function to add all prices
 function updateTotalCost() {
-    bestPrice = document.getElementById('best-price').innerText;
-    memoryCost = document.getElementById('memory-cost').innerText;
-    storageCost = document.getElementById('storage-cost').innerText;
-    deliveryCost = document.getElementById('delivery-cost').innerText;
-    totalCost = parseFloat(bestPrice) + parseFloat(memoryCost) + parseFloat(storageCost) + parseFloat(deliveryCost);
+
+    // getting all the costs 
+    const bestPrice = document.getElementById('best-price').innerText;
+    const memoryCost = document.getElementById('memory-cost').innerText;
+    const storageCost = document.getElementById('storage-cost').innerText;
+    const deliveryCost = document.getElementById('delivery-cost').innerText;
+
+    // adding all the costs
+    const totalCost = parseFloat(bestPrice) + parseFloat(memoryCost) + parseFloat(storageCost) + parseFloat(deliveryCost);
+
+    // updating the total price amount
     document.getElementById('total-cost').innerText = totalCost;
+
+    // updating the total amount
+    document.getElementById('final-total').innerText = totalCost;
 }
 
+
+// calling function for updating the parts' cost on click
 updatePartsCost( 'first-memory', 'memory-cost', 0 );
 updatePartsCost( 'second-memory', 'memory-cost', 180 );
 updatePartsCost( 'first-storage', 'storage-cost', 0 );
@@ -26,3 +40,32 @@ updatePartsCost( 'second-storage', 'storage-cost', 100 );
 updatePartsCost( 'third-storage', 'storage-cost', 180 );
 updatePartsCost( 'first-delivery', 'delivery-cost', 0 );
 updatePartsCost( 'second-delivery', 'delivery-cost', 20 );
+
+
+// updating the total on application of the promo code
+document.getElementById( 'apply-promo' ).addEventListener( 'click', function() {
+    const promoInput = document.getElementById( 'promo-text' );
+    const promoText = promoInput.value;
+    const promoCode = 'stevekaku';
+
+    // checking if the applied promo matches the correct promo
+    if( promoText == promoCode ) {
+
+        // geting the total price amount
+        const totalCost = document.getElementById('total-cost').innerText;
+
+        // applying discount
+        const discount = totalCost * 0.2;
+        const updatedTotalCost = totalCost - discount;
+
+        // updating the total amount
+        document.getElementById('final-total').innerText = updatedTotalCost;
+
+        // disabling the apply button
+        document.getElementById('apply-promo').disabled = true;
+        document.getElementById('apply-promo').style.backgroundColor = "gray";
+    }
+    
+    // clearing input 
+    promoInput.value = '';
+} )
